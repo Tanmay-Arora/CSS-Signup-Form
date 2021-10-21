550034691a6219c281f711b79e7977ecae563190
Tanmay-Arora <tanmay.179301213@gmail.com>
2021-10-21 11:34:16 +0530

    Animation Time fix

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function flip() {
     $('.card').toggleClass('flipped');
+    setTimeout(() => {
+        $('.front').toggleClass('front-disp');
+        $('.back').toggleClass('back-disp');    
+    }, 100);
+
+/*
     $('.front').toggleClass('front-disp');
     $('.back').toggleClass('back-disp');
+*/    
 }
